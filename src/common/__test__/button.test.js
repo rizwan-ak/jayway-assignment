7625f517a1a3be16e597c64cf8cfd3fa48dc49be
rizwan-ak <rizwan.ak@outlook.com>
2022-01-09 00:45:34 +0100

    add unit-test:button-click

--- src/common/__test__/button.test.js
+++ src/common/__test__/button.test.js
@@ -1,13 +1,26 @@
 import React from "react";
 import JWButton from "../components/JWButton";
 
-import { cleanup, render } from "@testing-library/react";
+import { cleanup, render, fireEvent } from "@testing-library/react";
 
 describe("JWButton", () => {
   afterEach(cleanup);
 
+  const handleButtonClick = jest.fn();
+
   test("pass:renders Button component", () => {
     const button = render(<JWButton />);
     expect(button).toMatchSnapshot();
+  });
+
+  test("pass:handleButtonClick called 1 time ", () => {
+    const { queryByText } = render(
+      <JWButton handleButtonClick={handleButtonClick} value={"Load More"} />
+    );
+    const button = queryByText("Load More");
+    // console.log(`button`, button);
+    fireEvent.click(button);
+
+    expect(handleButtonClick).toHaveBeenCalledTimes(1);
   });
 });
